60d7960699e7da067a1beaf3fe87ac3f177824b8
samifeadike <samifeadike@gmail.com>
2022-10-18 20:17:52 -0400

    modified the code as per @demo-25

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+        String name = "sam";
 
+		System.out.printIn(name);
 }
