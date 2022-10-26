1f8052912fd73a23f568e9fa6f33a6daabb42651
samifeadike <samifeadike@gmail.com>
2022-10-25 21:47:19 -0400

    fix bug

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,12 @@
-package com.example.demo;
 
+package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
-		String name = "sam";
-
-		System.out.println(name);
-	}
-
-		Integer age = "20";
-		System.out.printIn(age + 5);
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        String name = "sam";
+        System.out.println(name);
+    }
 }
